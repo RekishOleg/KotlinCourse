33c2accfbdbd9b66f64c1916aaa309851483fd89
Oleg Rekish <oleg.rekish@mail.ru>
2026-07-28 02:45:34 +0300

    KS-3-2 fix parentheses

--- src/main/kotlin/lesson_3/task2.kt
+++ src/main/kotlin/lesson_3/task2.kt
@@ -1,12 +1,12 @@
 package lesson_3
 
-fun main(){
+fun main() {
     val name = "Татьяна"
     val surname = "Сергеевна"
     val secondName = "Андреева"
     val secondNameAfterChange = "Сидорова"
     val ageBeforeChange = 20
     val ageAfterChange = 22
-    println("$secondName $name $surname, $ageBeforeChange")
-    println("$secondNameAfterChange $name $surname, $ageAfterChange")
+    println("[$secondName $name $surname, $ageBeforeChange]")
+    println("[$secondNameAfterChange $name $surname, $ageAfterChange]")
 }
