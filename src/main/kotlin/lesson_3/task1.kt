4d9a0819f96a91bc61200ba93bbe6952ede120d7
Oleg Rekish <oleg.rekish@mail.ru>
2026-07-27 22:37:18 +0300

    KS-3-1 add Name and age

---
+++ src/main/kotlin/lesson_3/task1.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main(){
+    val name = "Татьяна"
+    val surname = "Сергеевна"
+    val secondName = "Андреева"
+    val secondNameAfterChange = "Сидорова"
+    val ageBeforeChange = 20
+    val ageAfterChange = 22
+    println("$secondName $name $surname, $ageBeforeChange")
+    println("$secondNameAfterChange $name $surname, $ageBeforeChange")
+}
