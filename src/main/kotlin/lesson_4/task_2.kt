5bd61d2be932166805d9a7a7e9fb14df796d9ad4
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-01 01:15:51 +0300

    KS-4-2 fix formating

--- src/main/kotlin/lesson_4/task_2.kt
+++ src/main/kotlin/lesson_4/task_2.kt
@@ -1,11 +1,17 @@
 package lesson_4
 
-fun main(){
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
+const val minWeight = 35
+const val maxWeight = 100
+const val maxVolume = 100
 
-    println("Груз с весом 20 кг и объемом 80 л соответствует категории 'Average': ${20 > minWeight && 20 <= maxWeight && 80 < maxVolume}")
-    println("Груз с весом 50 кг и объемом 100 л соответствует категории 'Average': ${50 > minWeight && 50 <= maxWeight && 100 < maxVolume}")
+fun main() {
+    println(
+        "Груз с весом 20 кг и объемом 80 л соответствует категории" +
+                " 'Average': ${20 > minWeight && 20 <= maxWeight && 80 < maxVolume}"
+    )
+    println(
+        "Груз с весом 50 кг и объемом 100 л соответствует категории" +
+                " 'Average': ${50 > minWeight && 50 <= maxWeight && 100 < maxVolume}"
+    )
 
 }
