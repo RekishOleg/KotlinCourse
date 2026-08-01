1aec69edbe99331d1739c10f7aa17dc591c264a3
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-02 01:12:58 +0300

    KS-4-5 fix day of training and column position

--- src/main/kotlin/lesson_4/task_4.kt
+++ src/main/kotlin/lesson_4/task_4.kt
@@ -1,16 +1,13 @@
 package lesson_4
 
 fun main() {
-    val dayOfTraining = 2
+    val dayOfTraining = 5
     println(
         """
-        Упражнения для рук: ${dayOfTraining % 2 != 0}
-
-        Упражнения для ног: ${dayOfTraining % 2 == 0}
-
-        Упражнения для спины: ${dayOfTraining % 2 == 0}
-
-        Упражнения для пресса: ${dayOfTraining % 2 != 0}
-    """.trimIndent()
+        |Упражнения для рук: ${dayOfTraining % 2 != 0}
+        |Упражнения для ног: ${dayOfTraining % 2 == 0}
+        |Упражнения для спины: ${dayOfTraining % 2 == 0}
+        |Упражнения для пресса: ${dayOfTraining % 2 != 0}
+    """.trimMargin()
     )
 }
