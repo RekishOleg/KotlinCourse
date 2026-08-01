3caea239594d32426ff56bbd4d19d086d289d942
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-02 01:23:01 +0300

    KS-4-5 fix true/false position

--- src/main/kotlin/lesson_4/task_4.kt
+++ src/main/kotlin/lesson_4/task_4.kt
@@ -4,9 +4,9 @@ fun main() {
     val dayOfTraining = 5
     println(
         """
-        |Упражнения для рук: ${dayOfTraining % 2 != 0}
-        |Упражнения для ног: ${dayOfTraining % 2 == 0}
-        |Упражнения для спины: ${dayOfTraining % 2 == 0}
+        |Упражнения для рук:    ${dayOfTraining % 2 != 0}
+        |Упражнения для ног:    ${dayOfTraining % 2 == 0}
+        |Упражнения для спины:  ${dayOfTraining % 2 == 0}
         |Упражнения для пресса: ${dayOfTraining % 2 != 0}
     """.trimMargin()
     )
