eb95e1c4cba8902dd62bfdcb607e89fae891c4f0
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-04 01:34:23 +0300

    KS-5-3 fix text output

--- src/main/kotlin/lesson_5/task_3.kt
+++ src/main/kotlin/lesson_5/task_3.kt
@@ -2,17 +2,16 @@ package lesson_5
 
 const val CORRECT_NUM_ONE = 6
 const val CORRECT_NUM_TWO = 7
-
 fun main() {
-    print("Enter your numbers: ")
+    println("Введите ваши числа: ")
     val num1 = readln().toInt()
     val num2 = readln().toInt()
-    print("Correct numbers: 6, 7")
+    println("Правильные числа: 6, 7")
     if (num1 == CORRECT_NUM_ONE && num2 == CORRECT_NUM_TWO || num1 == CORRECT_NUM_TWO && num2 == CORRECT_NUM_ONE) {
-        println("Congratulations! You won the main prize!")
+        println("Поздравляем! Вы выиграли главный приз!")
     } else if (num1 == CORRECT_NUM_ONE || num2 == CORRECT_NUM_TWO || num1 == CORRECT_NUM_TWO || num2 == CORRECT_NUM_ONE) {
-        println("You won a consolation prize")
+        println("Вы выиграли утешительный приз")
     } else {
-        println("Failure!")
+        println("Неудача!")
     }
 }
