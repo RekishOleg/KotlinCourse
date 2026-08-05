70149f7b624cc0cf89b680e8cfed34c03c699abb
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-06 01:15:50 +0300

    KS-5-4 add verification for the spaceship

---
+++ src/main/kotlin/lesson_5/task_4.kt
@@ -0,0 +1,18 @@
+﻿package lesson_5
+const val NAME_OF_USER_1 = "Zaphod"
+const val PASSWORD_OF_USER_1 = "PanGalactic"
+fun main(){
+
+    println("Введите имя пользователя:")
+    val nameOfCurrentUser = readln()
+
+    when(nameOfCurrentUser){
+        NAME_OF_USER_1 -> println("Пользователь найден, введите пароль")
+        else -> println("Пользователь не найден, пройдите верификацию")
+    }
+    val passwordOfCurrentUser = readln()
+    when(passwordOfCurrentUser){
+        PASSWORD_OF_USER_1 -> println("Добро пожаловать, $nameOfCurrentUser")
+        else -> println("Пароль неверный, попробуйте еще раз")
+    }
+}
