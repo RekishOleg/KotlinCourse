872852c1105cecdf1eace706015766ba9d3cd30d
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-06 18:35:44 +0300

    KS-5-5 add app authorization

---
+++ src/main/kotlin/lesson_6/task_1.kt
@@ -0,0 +1,17 @@
+﻿package lesson_6
+
+fun main() {
+    println("Создайте логин и пароль:")
+    val loginOfUser = readln()
+    val passwordOfUser = readln()
+    println("Введите логин и пароль чтобы войти в приложение:")
+    val enteredLogin = readln()
+    val enteredPassword = readln()
+    while (loginOfUser !=enteredLogin && passwordOfUser != enteredPassword) {
+        println("Логин или пароль введён не верно")
+        println("Введите логин и пароль чтобы войти в приложение:")
+        val enteredLogin = readln()
+        val enteredPassword = readln()
+    }
+    println("Авторизация прошла успешно")
+}
