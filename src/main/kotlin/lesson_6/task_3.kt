cf76da1c1d009a0e98e39011e53cfbbd9272f0d5
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-20 23:34:45 +0300

    KS-6-2 fix do while to while

--- src/main/kotlin/lesson_6/task_3.kt
+++ src/main/kotlin/lesson_6/task_3.kt
@@ -4,13 +4,13 @@ fun main() {
     println("Введите кол-во секунд для отсчета:")
     var numberOfSeconds = readln().toInt()
 
-    do {
+    while (numberOfSeconds > 0) {
         println("Осталось секунд: ${numberOfSeconds}")
         Thread.sleep(1000)
         numberOfSeconds--
 
 
-    } while (numberOfSeconds > 0)
+    }
 
     println("Время вышло")
 }
