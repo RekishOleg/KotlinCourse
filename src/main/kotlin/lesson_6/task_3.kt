7391b079145b218e704886f32bd9678956268fc0
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-20 23:21:19 +0300

    KS-6-2 add countdown timer

---
+++ src/main/kotlin/lesson_6/task_3.kt
@@ -0,0 +1,16 @@
+﻿package lesson_6
+
+fun main() {
+    println("Введите кол-во секунд для отсчета:")
+    var numberOfSeconds = readln().toInt()
+
+    do {
+        println("Осталось секунд: ${numberOfSeconds}")
+        Thread.sleep(1000)
+        numberOfSeconds--
+
+
+    } while (numberOfSeconds > 0)
+
+    println("Время вышло")
+}
