cd42530fe2d085552952797f53daaeaa115317b0
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-21 21:14:25 +0300

    KS-6-4 add console-based "guess the number" game

---
+++ src/main/kotlin/lesson_6/task_4.kt
@@ -0,0 +1,23 @@
+﻿package lesson_6
+
+fun main() {
+    val range = 1..9
+    val randomNumber = range.random()
+    var numberOfAttempts = 0
+
+    while (true) {
+        println("Введите число от 1 до 9")
+        val userNumber = readln().toInt()
+        if (userNumber == randomNumber) {
+            println("Это была великолепная игра!")
+            break
+        } else {
+            println("Неверно")
+        }
+        numberOfAttempts++
+        if (numberOfAttempts == 5) {
+            println("Было загадано число $randomNumber")
+            break
+        }
+    }
+}
