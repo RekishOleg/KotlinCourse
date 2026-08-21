5198919a64f61ff4815e97fead4bca22ab87f007
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-21 21:21:16 +0300

    KS-6-4 add displaying attempts

--- src/main/kotlin/lesson_6/task_4.kt
+++ src/main/kotlin/lesson_6/task_4.kt
@@ -3,19 +3,21 @@
 fun main() {
     val range = 1..9
     val randomNumber = range.random()
-    var numberOfAttempts = 0
+    var numberOfAttempts = 5
 
     while (true) {
+        numberOfAttempts--
         println("Введите число от 1 до 9")
         val userNumber = readln().toInt()
+
         if (userNumber == randomNumber) {
             println("Это была великолепная игра!")
             break
         } else {
-            println("Неверно")
+            println("Неверно \n Осталось попыток: $numberOfAttempts ")
         }
-        numberOfAttempts++
-        if (numberOfAttempts == 5) {
+
+        if (numberOfAttempts == 0) {
             println("Было загадано число $randomNumber")
             break
         }
