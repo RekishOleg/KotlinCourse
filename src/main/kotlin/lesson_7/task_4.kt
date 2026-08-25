cec29be8d5db87e5ffc584367b68bdc07dc34906
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-25 20:12:23 +0300

    KS-7-4 add timer

---
+++ src/main/kotlin/lesson_7/task_4.kt
@@ -0,0 +1,12 @@
+﻿package lesson_7
+
+fun main() {
+    println("Введите кол-во секунд которое нужно засечь:")
+    val userNumber = readln().toInt()
+
+    for (i in 1..userNumber) {
+        Thread.sleep(1000)
+        println(i)
+    }
+    println("Время вышло")
+}
