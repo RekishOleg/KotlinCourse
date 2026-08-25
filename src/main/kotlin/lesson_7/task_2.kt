9f092f081202b19cc7e386e88bda7d50fffcfac5
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-25 17:12:31 +0300

    KS-7-2 add sms based authorization

---
+++ src/main/kotlin/lesson_7/task_2.kt
@@ -0,0 +1,13 @@
+﻿package lesson_7
+
+fun main() {
+    do {
+        val smsCode = (1000..9999).random().toString()
+        println("Ваш код для авторизации: $smsCode")
+        val userCode = readln()
+        if (userCode == smsCode) {
+            println("Добро пожаловать!")
+        }
+    } while (userCode != smsCode)
+
+}
