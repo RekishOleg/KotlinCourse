688c9473a58267b37626672eeac090e353062f71
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-25 16:19:20 +0300

    KS-7-1 add generator of passwords

---
+++ src/main/kotlin/lesson_7/task_1.kt
@@ -0,0 +1,14 @@
+﻿package lesson_7
+
+import kotlin.random.Random
+
+fun main() {
+    val alphabet = ('a'..'z').toList()
+    val numbers = ('0'..'9').toList()
+    var password = ""
+    for (i in 1..3) {
+        password += alphabet[Random.nextInt(alphabet.size)]
+        password += numbers[Random.nextInt(numbers.size)]
+    }
+    println(password)
+}
