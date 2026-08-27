bafef6e245469b7675c4ffdfae5af32ff60a7c69
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-28 00:10:43 +0300

    KS-8-2 add checking an ingredient in a recipe with complications

---
+++ src/main/kotlin/lesson_8/task_3.kt
@@ -0,0 +1,14 @@
+﻿package lesson_8
+
+fun main() {
+    val arrayOfIngredients = arrayOf("Спагетти", "Бекон", "Пармезан", "Яйцо")
+
+    println("Какой ингредиент вам нужен?")
+    val userIngredient = readln()
+
+    if (arrayOfIngredients.contains(userIngredient)) {
+        println("Ингредиент ${userIngredient} в рецепте есть")
+    } else {
+        println("Такого ингредиента в рецепте нет")
+    }
+}
