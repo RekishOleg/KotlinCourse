467638f09f19b0f7860deb7d55d77f2161f49f8d
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-28 00:49:46 +0300

    KS-8-4 fix out of fun

--- src/main/kotlin/lesson_8/task_4.kt
+++ src/main/kotlin/lesson_8/task_4.kt
@@ -1,17 +1,14 @@
 ﻿package lesson_8
 
-fun main() {
+fun main(){
     val arrayOfIngredient = arrayOf("Яйца", "Бекон", "Зелень", "Лук")
     println(arrayOfIngredient.joinToString(", "))
-    var userIngredientToDelete = ""
 
-    while (!arrayOfIngredient.contains(userIngredientToDelete)) {
-        println("Какой ингредиент заменить?")
-        userIngredientToDelete = readln()
-        if (!arrayOfIngredient.contains(userIngredientToDelete)) {
-            println("Такого ингредиента нет!")
-
-        }
+    println("Какой ингредиент заменить?")
+    val userIngredientToDelete = readln()
+    if (!arrayOfIngredient.contains(userIngredientToDelete)){
+        println("Такого ингредиента нет!")
+        return
     }
 
     println("Какой ингредиент добавить?")
