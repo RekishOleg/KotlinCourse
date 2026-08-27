8187bba1c3224f2bbceedbd909cb258c2ae5a86d
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-28 00:41:39 +0300

    KS-8-4 add change to the list of ingredients

---
+++ src/main/kotlin/lesson_8/task_4.kt
@@ -0,0 +1,23 @@
+﻿package lesson_8
+
+fun main() {
+    val arrayOfIngredient = arrayOf("Яйца", "Бекон", "Зелень", "Лук")
+    println(arrayOfIngredient.joinToString(", "))
+    var userIngredientToDelete = ""
+
+    while (!arrayOfIngredient.contains(userIngredientToDelete)) {
+        println("Какой ингредиент заменить?")
+        userIngredientToDelete = readln()
+        if (!arrayOfIngredient.contains(userIngredientToDelete)) {
+            println("Такого ингредиента нет!")
+
+        }
+    }
+
+    println("Какой ингредиент добавить?")
+    val userIngredientToAdd = readln()
+
+    arrayOfIngredient[arrayOfIngredient.indexOf(userIngredientToDelete)] = userIngredientToAdd
+
+    println("Готово! Вы сохранили следующий список: ${arrayOfIngredient.joinToString(", ")}")
+}
