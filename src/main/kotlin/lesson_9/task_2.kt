b4799153c9d78fa1aa883e2ccafec9a424e7bbdc
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-30 22:44:11 +0300

    KS-9-1 add user ingredient

---
+++ src/main/kotlin/lesson_9/task_2.kt
@@ -0,0 +1,20 @@
+﻿package lesson_9
+
+fun main() {
+    val listOfIngredients = mutableListOf("Яйцо", "Помидор", "Бекон")
+    println("В рецепте есть базовые ингредиенты: ${listOfIngredients.joinToString(", ")}")
+    println("Желаете добавить еще?")
+    val userAnswer = readln()
+
+    if (userAnswer.equals("ДА", ignoreCase = true)) {
+        println("Какой ингредиент вы хотите добавить?")
+
+    } else {
+        return
+    }
+
+    val userIngredient = readln()
+    listOfIngredients.add(userIngredient)
+    println("Теперь в рецепте есть следующие ингредиенты: ${listOfIngredients.joinToString(", ")}")
+
+}
