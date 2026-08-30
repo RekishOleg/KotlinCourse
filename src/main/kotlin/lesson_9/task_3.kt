3b0c271bed9eca58c5af4f24ff574de4c9ad8792
Oleg Rekish <oleg.rekish@mail.ru>
2026-08-30 23:32:06 +0300

    KS-9-3 add portion calculation

---
+++ src/main/kotlin/lesson_9/task_3.kt
@@ -0,0 +1,12 @@
+﻿package lesson_9
+
+fun main() {
+    val listOfIngredients = mutableListOf(2, 50, 15)
+    println("Сколько нужно порций?")
+    val userAnswer = readln().toInt()
+    val userListOfIngredients = listOfIngredients.map { it * userAnswer }
+    println(
+        "На $userAnswer порций вам понадобится: Яиц – ${userListOfIngredients[0]}," +
+                " молока – ${userListOfIngredients[1]}, сливочного масла – ${userListOfIngredients[2]}"
+    )
+}
