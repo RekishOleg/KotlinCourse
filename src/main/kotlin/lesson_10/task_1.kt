52eef7d8803bb5719e9bd53e4a1b08209ba6ac25
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-02 00:06:54 +0300

    KS-9-3 add die roll

---
+++ src/main/kotlin/lesson_10/task_1.kt
@@ -0,0 +1,19 @@
+﻿package lesson_10
+
+fun diceRoll(name: String): Int {
+    val randomNumber = (1..6).random()
+    println("$name бросил: $randomNumber")
+    return randomNumber
+}
+
+fun main() {
+    val userMove = diceRoll(name = "Игрок")
+    val computerMove = diceRoll(name = "Компьютер")
+    if (userMove > computerMove) {
+        println("Победило человечество")
+    } else if (userMove < computerMove) {
+        println("Победила машина")
+    } else {
+        println("Победила дружба")
+    }
+}
