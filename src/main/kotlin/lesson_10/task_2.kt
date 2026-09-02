f66fe12dce0231c6afa8a1e946fb41ba95560a84
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-02 23:31:07 +0300

    KS-10-2 add hello message

--- src/main/kotlin/lesson_10/task_2.kt
+++ src/main/kotlin/lesson_10/task_2.kt
@@ -3,6 +3,8 @@
 fun validateLogin(login: String, password: String) {
     if (login.length < 4 || password.length < 4) {
         println("Логин или пароль недостаточно длинные")
+    } else {
+        println("Добро подаловать, $login!")
     }
 }
 
