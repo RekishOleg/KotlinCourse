22a9123894927fdca7e329b5b7bcf066d867aea7
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-02 23:27:00 +0300

    KS-10-2 add login validation

---
+++ src/main/kotlin/lesson_10/task_2.kt
@@ -0,0 +1,17 @@
+﻿package lesson_10
+
+fun validateLogin(login: String, password: String) {
+    if (login.length < 4 || password.length < 4) {
+        println("Логин или пароль недостаточно длинные")
+    }
+}
+
+fun main() {
+    println("Введите логин")
+    val userLogin = readln()
+    println("Введите пароль")
+    val userPassword = readln()
+
+    validateLogin(login = userLogin, password = userPassword)
+
+}
