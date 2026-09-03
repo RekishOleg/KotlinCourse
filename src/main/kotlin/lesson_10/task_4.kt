19b71d0ff55d19a0e21516173a642d52804afe5c
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-03 19:32:53 +0300

    KS-10-4 add enhanced dice game

---
+++ src/main/kotlin/lesson_10/task_4.kt
@@ -0,0 +1,35 @@
+﻿package lesson_10
+
+fun randomNumber(): Int {
+    val randomNumber = (1..6).random()
+    return randomNumber
+}
+
+fun diceRoll(): Boolean {
+    val computerRoll = randomNumber()
+    val userRoll = randomNumber()
+    var isUserWins = false
+    if (userRoll > computerRoll) {
+        println("Победило человечество")
+        isUserWins = true
+    } else if (userRoll < computerRoll) {
+        println("Победила машина")
+    } else {
+        println("Победила дружба")
+    }
+    return isUserWins
+}
+
+fun main() {
+    var winsOfUser = 0
+    var userAnswer = ""
+    do {
+        if (diceRoll()){
+            winsOfUser += 1
+        }
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+         userAnswer = readln()
+
+    }while (userAnswer == "Да")
+    println(winsOfUser)
+}
