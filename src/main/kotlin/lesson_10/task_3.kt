943bd397e76f99062ea7b976215567091e663474
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-03 18:51:38 +0300

    KS-10-3 add password generator

---
+++ src/main/kotlin/lesson_10/task_3.kt
@@ -0,0 +1,22 @@
+﻿package lesson_10
+
+fun generatePassword(length: Int = 8): String {
+    val numbers = '0'..'9'
+    val specialChars = "!\"#$%&'()*+,-./"
+    var password = ""
+    for (i in 1..length / 2) {
+        password += numbers.random()
+        password += specialChars.random()
+
+    }
+    if (length % 2 != 0) {
+        password += numbers.random()
+    }
+    return password
+}
+
+fun main() {
+    println("Введите длину пароля")
+    val password = generatePassword(length = readln().toInt())
+    println(password)
+}
