6684592aaa8f68dd5b0a5bef46731915fbb3ca30
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-05 22:08:29 +0300

    KS-10-5 add user model

---
+++ src/main/kotlin/lesson_11/task_1.kt
@@ -0,0 +1,16 @@
+﻿package lesson_11
+
+class User (
+    val id: Int,
+    val login: String,
+    val password: String,
+    val mail: String,
+)
+
+fun main() {
+    val user1 = User(1, "John", "123", "secret@gmail.com")
+    val user2 = User(2, "Oleg", "456", "banana@gmail.com")
+    println("${user1.id}, ${user1.login}, ${user1.password}, ${user1.mail}")
+    println("${user2.id}, ${user2.login}, ${user2.password}, ${user2.mail}")
+
+}
