b3aabde19ab043a3eda002ce77d3d6f92163c7fa
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-06 22:13:32 +0300

    KS-11-4 add description of classes

---
+++ src/main/kotlin/lesson_11/task_4.kt
@@ -0,0 +1,21 @@
+﻿package lesson_11
+
+class RecipeCategory(
+    val name: String,
+    val description: String,
+    val imageUrl: String,
+)
+
+class Recipe(
+    val name: String,
+    val numberOfServings: Int,
+    val preparationMethods: List<String>,
+    val ingredients: List<Ingredient>,
+    val category: RecipeCategory,
+    val imageUrl: String,
+)
+
+class Ingredient(
+    val name: String,
+    val amount: String,
+)
