7fae80a3e76a3cf305b3ead1fae45fc9eb595849
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-15 20:36:30 +0300

    KS-12-2 add weather with kelvins

---
+++ src/main/kotlin/lesson_12/task_3.kt
@@ -0,0 +1,26 @@
+﻿package lesson_12
+
+
+class WeatherWithKelvins(
+    dayTemperature: Int,
+    nightTemperature: Int,
+    isPrecipitation: Boolean
+) {
+    val dayTemperatureCelsius: Int = dayTemperature - 273
+    val nightTemperatureCelsius: Int = nightTemperature - 273
+    val isPrecipitation: Boolean = isPrecipitation
+
+    fun printValues() {
+        println(
+            "Day temperature: $dayTemperatureCelsius," +
+                    " night temperature: $nightTemperatureCelsius," +
+                    " isPrecipitation: $isPrecipitation"
+        )
+    }
+}
+
+fun main() {
+    val weather = WeatherWithKelvins(10, 5, false)
+    weather.printValues()
+
+}
