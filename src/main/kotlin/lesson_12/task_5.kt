c9be3f19bb395502efc51d5420e54c8c27e7e197
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-16 21:15:03 +0300

    KS-12-5 add statistics of weather

---
+++ src/main/kotlin/lesson_12/task_5.kt
@@ -0,0 +1,39 @@
+﻿package lesson_12
+
+import kotlin.random.Random
+
+
+class StatisticsOfWeather(
+    val dayTemperature: Int,
+    val nightTemperature: Int,
+    val isPrecipitation: Boolean,
+)
+
+fun main() {
+    val rangeOfNumbersForDay = 10..20
+    val rangeOfNumbersForNight = -10..9
+    val listOfWeather = mutableListOf<StatisticsOfWeather>()
+    for (i in 1..30) {
+        listOfWeather.add(
+            StatisticsOfWeather(
+                dayTemperature = rangeOfNumbersForDay.random(),
+                nightTemperature = rangeOfNumbersForNight.random(),
+                isPrecipitation = Random.nextBoolean()
+            )
+        )
+    }
+    val listOfDayTemperature = listOfWeather.map { it.dayTemperature }
+    val listOfNightTemperature = listOfWeather.map { it.nightTemperature }
+    val countOfPrecipitations = listOfWeather.count { it.isPrecipitation == 1 }
+    val averageDayTemperature = listOfDayTemperature.average()
+    val averageNightTemperature = listOfNightTemperature.average()
+    println(
+        """
+        Кол-во дней с осадками: $countOfPrecipitations
+        Средняя дневная температура: $averageDayTemperature
+        Средняя ночная температура: $averageNightTemperature
+    """.trimIndent()
+    )
+
+
+}
