e599e5fdaddd65c0558af02d1b9bf95167e8597b
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-18 15:28:42 +0300

    KS-13-1 add telephone directory

---
+++ src/main/kotlin/lesson_13/task_1.kt
@@ -0,0 +1,12 @@
+﻿package lesson_13
+
+class TelephoneDirectory(
+    val name: String,
+    val phoneNumber: Long,
+    val companyName: String?,
+)
+
+fun main() {
+    val telephoneDirectory = TelephoneDirectory("Oleg", 123, null)
+    println(telephoneDirectory.companyName)
+}
