5098a695c1ebca9634dcc8a4a583bc3d39f49b5c
Oleg Rekish <oleg.rekish@mail.ru>
2026-09-19 14:35:37 +0300

    KS-13-3 add telephone directory with default values

--- src/main/kotlin/lesson_13/task_3.kt
+++ src/main/kotlin/lesson_13/task_3.kt
@@ -1,2 +1,29 @@
 ﻿package lesson_13
 
+
+class TelephoneDirectoryWithList(
+    val name: String,
+    val phoneNumber: Long,
+    val companyName: String? = null,
+) {
+    fun printPhoneInfo() {
+        println(
+            "Имя: $name\n" +
+                    "Номер: $phoneNumber\n" +
+                    "Компания: ${companyName ?: "<не указано>"}"
+        )
+    }
+}
+
+fun main() {
+    val listOfPhoneNumbers: MutableList<TelephoneDirectoryWithList> =
+        mutableListOf(
+            TelephoneDirectoryWithList("Alex", 1, null),
+            TelephoneDirectoryWithList("Bob", 2, null),
+            TelephoneDirectoryWithList("", 3, "null"),
+            TelephoneDirectoryWithList("Alice", 4, "kaka"),
+            TelephoneDirectoryWithList("Magamed", 5, "LOL")
+        )
+    println(listOfPhoneNumbers.map { it.companyName }.filterNotNull().joinToString("\n"))
+
+}
